05d82c1105ea01b84f28cbd9913d5a6d69d0673c
Ken Snyder <ken@ken.net>
2015-05-12 10:42:48 +0100

    created addon-soecific ember-try config

---
+++ config/ember-try.js
@@ -0,0 +1,22 @@
+module.exports = {
+  scenarios: [
+    {
+      name: 'Ember Release',
+      dependencies: {
+        'ember': 'release'
+      }
+    },
+    {
+      name: 'Ember 1.11.3',
+      dependencies: {
+        'ember': '1.11.3'
+      }
+    },
+    {
+      name: 'Ember canary',
+      dependencies: {
+        'ember': 'canary'
+      }
+    }
+  ]
+};
